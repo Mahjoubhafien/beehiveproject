6147eff44d537dc602cd691e0cff58c95536aed1
Mahjoubhafyen <mahjoubhafyen@gmail.com>
2025-05-17 10:12:51 +0100

    update all hive page with one hive card with hard codded data sensors

--- src/views/admin/all-hives/index.jsx
+++ src/views/admin/all-hives/index.jsx
@@ -1,6 +1,25 @@
+import React, { useState } from "react";
+import hiveData from "./variables/hiveData.js";
+import HiveCard from "./components/HiveCard.jsx";
+import NFt2 from "assets/img/nfts/Nft2.png";
+import NFt4 from "assets/img/nfts/Nft4.png";
+import NFt3 from "assets/img/nfts/Nft3.png";
+
 const AllHives = () => {
-  return (<h1>test</h1>
-  )
+  return (
+    <div className="z-20 grid grid-cols-1 gap-5 md:grid-cols-4">
+    <HiveCard
+      hiveName="Test hive 1"
+      id="1"
+      image={NFt2}
+      Temperature="27"
+      Humidity="75"
+      lastDataR="2025/05/2025 08:56"
+      link="default"
+      //extra="max-w-[250px]"
+    />
+    </div>
+  );
 };
 
 export default AllHives;
---
+++ src/views/admin/all-hives/variables/hiveData.js
@@ -0,0 +1,22 @@
+var hiveData = [
+  {
+    id: 1,
+    hiveName:"Test hive 1",
+    lastDataTime: "17/05/2025 07:27",
+    temperature: 24,
+    humidity: 75,
+    longitude:1000,
+    latitude:2000
+  },
+  {
+    id: 2,
+    hiveName:"Test hive 2",
+    lastDataTime: "17/05/2025 07:27",
+    temperature: 26,
+    humidity: 85,
+    longitude:1500,
+    latitude:2300
+  }
+];
+
+export default hiveData;
