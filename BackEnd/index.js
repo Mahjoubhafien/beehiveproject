daf7b77c50a6bb223d07a5b6c1c30189e451601e
Mahjoubhafyen <mahjoubhafyen@gmail.com>
2025-05-18 15:43:29 +0100

    the adding and show hives work need more work ofc

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -7,7 +7,7 @@ import cors from "cors";
 
 const app = express();
 const port = 5000;
-const userId = 1;
+const currentUserId = 1;
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
@@ -31,13 +31,22 @@ app.get("/admin/all-hives", async (req, res) => {
     console.log("i got triggered");
 res.json({ message: "Hello from server" });  // ✅ this is JSON
 });
-//Adding new hive to beehives table
+/*
+Adding new hive to beehives table
+*/
 app.post("/admin/add-hive", async (req, res) => {
     const { hiveName, hiveLocation, hiveNumber } = req.body;
 try {
     const result = await db.query(
       'INSERT INTO beehives (user_id, hive_name, location, hive_number) VALUES ($1, $2, $3, $4) RETURNING *',
-      [userId, hiveName, hiveLocation, hiveNumber]
+      [currentUserId, hiveName, hiveLocation, hiveNumber]
+    );
+    // Access hive_number of the newly inserted row
+    const newHive = result.rows[0];
+
+    await db.query(
+      'INSERT INTO sensor_data (beehive_id, temperature, humidity, longitude,latitude) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+      [newHive.id, null, null, null, null]
     );
     res.status(201).json(result.rows[0]);
   } catch (err) {
@@ -45,7 +54,38 @@ try {
     res.status(500).json({ error: 'Database error' });
   }
 });
+/*
+getting hive list of specific user id
+*/
+app.get("/admin/getAllHives", async (req, res) => {
+try {
+    const beehivesResult = await db.query(
+      'SELECT beehives.id from beehives where user_id = ($1)',
+      [currentUserId]
+    );
 
+    const beehiveIds = beehivesResult.rows.map(row => row.id);
+    console.log(beehiveIds);
+
+   // Get the latest sensor_data for each beehive_id
+    const sensorData = await db.query(
+      `
+      SELECT DISTINCT ON (beehive_id) *
+      FROM sensor_data
+      WHERE beehive_id = ANY($1)
+      ORDER BY beehive_id, timestamp DESC
+      `,
+      [beehiveIds]
+    );
+
+    console.log("Latest Sensor Data:", sensorData.rows);
+
+    res.status(200).json(sensorData.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
